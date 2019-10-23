55f3751069cad2e4438dd2ae7c4e07c9c6af20de
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-10-22 18:31:33 -0700

    add variable with q&a's

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+var questionnaire = [
+    {
+        question: "Which is the correct term for Identical Twins?",
+        answerOptions: ["Monochorionic", "Monozygotic", "Dizygotic", "Polyzygotic"],
+        validAnswer: "Monozygotic"
+    },
+    {
+        question: "How many twins are born each year in the US?",
+        answerOptions: ["33.4 per 1,000 live births", "41.3 per 1,000 live births", "29.6 per 1,000 live births", "23.9 per 1,000 live births"],
+        validAnswer: "33.4 per 1,000 live births"
+    },
+    {
+        question: "Which state has the highest rate of twin births in the US?",
+        answerOptions: ["Illinois", "California", "New York", "Connecticut"],
+        validAnswer: "Connecticut"
+    },
+    {
+        question: "What is the origin of the word 'twin'?",
+        answerOptions: ["German", "Greek", "Latin", "Unknown"],
+        validAnswer: "German"
+    },
+    {
+        question: "Which celebrity has a twin sibling?",
+        answerOptions: ["Madonna", "Tom Cruise", "Scarlett Johansson", "Ariana Grande"],
+        validAnswer: "Scarlett Johansson"
+    }
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
